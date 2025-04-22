dae6d944159811dea465fb1092faf26f0b2c00f4
Murat Kasimov <iokasimov.m@gmail.com>
2025-04-22 09:28:29 +0700

    Redesign Monoidal Functors

--- Ya/World.hs
+++ Ya/World.hs
@@ -7,17 +7,17 @@ import "ghc-prim" GHC.Types (IO (IO))
 
 type World = IO
 
-instance Mapping U_I_II U_I_II Arrow Arrow World World
+instance Mapping U_I_II U_I_II AR AR World World
  where mapping = rewrap / \m x -> bindIO x (returnIO `fio` m)
 
-instance Mapping U_I_II U_I_II Arrow Arrow (World `T'TT'I` L () World) World
+instance Mapping U_I_II U_I_II AR AR (World `T'TT'I` Unit `L` World) World
  where mapping = rewrap / \m (T'TT'I ioio) -> bindIO (bindIO ioio unwrap) (returnIO `fio` m)
 
-instance Mapping U_I_II U_I_II Arrow Arrow (Day U_I_II Arrow P P World World i ii) World
- where mapping = rewrap / \from (U_V_UU_UUU_UUUU_T'TT'I_II_III (These (These x y) (U_I_II f))) -> bindIO x (\xx -> from `compose` f `compose` These xx `fo` y)
+instance Mapping U_I_II U_I_II AR AR (Day U_I_II AR P P World (Unit `L` World) i ii) World
+ where mapping = rewrap / \from (U_V_UU_UUU_UUUU_T'TT'I_II_III (These (These x (Labeled y)) (U_I_II f))) -> bindIO x (\xx -> from `compose` f `compose` These xx `fo` y)
 
-instance Mapping U_I_II U_I_II Arrow Arrow (U_I_II Arrow ()) World
- where mapping = rewrap / \from (U_I_II f) -> returnIO `ha` from `li` f ()
+instance Mapping U_I_II U_I_II AR AR (U_I_II AR Unit) World
+ where mapping = rewrap / \from (U_I_II f) -> returnIO `ha` from `li` f Unit
 
 returnIO :: a -> World a
 returnIO x = IO (\ s -> (# s, x #))
@@ -28,7 +28,8 @@ bindIO (IO m) k = IO (\s -> case m s of (# new_s, x #) -> unIO (k x) new_s)
 unIO :: World a -> (State# RealWorld -> (# State# RealWorld, a #))
 unIO (IO x) = x
 
-pattern Raw, Out, World :: World e -> L () World e
+pattern Raw, Out, World, Await :: World e -> (Unit `L` World) e
 pattern Raw e = Labeled e
 pattern Out e = Labeled e
 pattern World e = Labeled e
+pattern Await e = Labeled e

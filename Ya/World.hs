007fd2eb7c9baf91b881182eb266f6afce9dbc64
Murat Kasimov <iokasimov.m@gmail.com>
2025-06-10 20:29:05 +0700

    Remove ancient (/) operator

--- Ya/World.hs
+++ Ya/World.hs
@@ -8,16 +8,16 @@ import "ghc-prim" GHC.Types (IO (IO))
 type World = IO
 
 instance Mapping T'I'II T'I'II AR AR World World
- where mapping = rewrap / \m x -> bindIO x (returnIO `fio` m)
+ where mapping = rewrap `identity` \m x -> bindIO x (returnIO `fio` m)
 
 instance Mapping T'I'II T'I'II AR AR (World `T'TT'I` Void `L` World) World
- where mapping = rewrap / \m (T'TT'I ioio) -> bindIO (bindIO ioio unwrap) (returnIO `fio` m)
+ where mapping = rewrap `identity` \m (T'TT'I ioio) -> bindIO (bindIO ioio unwrap) (returnIO `fio` m)
 
 instance Mapping T'I'II T'I'II AR AR (Day T'I'II AR P P World (Void `L` World) i ii) World
- where mapping = rewrap / \from (U_V_UU_UUU_UUUU_T'TT'I_II_III (These (These x (Labeled y)) (T'I'II f))) -> bindIO x (\xx -> from `compose` f `compose` These xx `fo` y)
+ where mapping = rewrap `identity` \from (U_V_UU_UUU_UUUU_T'TT'I_II_III (These (These x (Labeled y)) (T'I'II f))) -> bindIO x (\xx -> from `compose` f `compose` These xx `fo` y)
 
 instance Mapping T'I'II T'I'II AR AR (T'I'II AR Unit) World
- where mapping = rewrap / \from (T'I'II f) -> returnIO `ha` from `li` f Unit
+ where mapping = rewrap `identity` \from (T'I'II f) -> returnIO `ha` from `li` f Unit
 
 returnIO :: a -> World a
 returnIO x = IO (\ s -> (# s, x #))

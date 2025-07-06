e88794783a210478ce5a77040e90af3bb291c514
Murat Kasimov <iokasimov.m@gmail.com>
2025-07-06 17:40:00 +0700

    Add phantom parameters to `Labeled` wrappers

--- Ya/World.hs
+++ Ya/World.hs
@@ -10,10 +10,10 @@ type World = IO
 instance Mapping T'I'II T'I'II AR AR World World
  where mapping = rewrap `identity` \m x -> bindIO x (returnIO `fio` m)
 
-instance Mapping T'I'II T'I'II AR AR (World `T'TT'I` Void `L` World) World
+instance Mapping T'I'II T'I'II AR AR (World `T'TT'I` World `L` World `T` Void) World
  where mapping = rewrap `identity` \m (T'TT'I ioio) -> bindIO (bindIO ioio unwrap) (returnIO `fio` m)
 
-instance Mapping T'I'II T'I'II AR AR (Day T'I'II AR P P World (Void `L` World) i ii) World
+instance Mapping T'I'II T'I'II AR AR (Day T'I'II AR P P World (World `L` World `T` Void) i ii) World
  where mapping = rewrap `identity` \from (U_V_UU_UUU_UUUU_T'TT'I_II_III (These (These x (Labeled y)) (T'I'II f))) -> bindIO x (\xx -> from `compose` f `compose` These xx `fo` y)
 
 instance Mapping T'I'II T'I'II AR AR (T'I'II AR Unit) World
@@ -28,7 +28,7 @@ bindIO (IO m) k = IO (\s -> case m s of (# new_s, x #) -> unIO (k x) new_s)
 unIO :: World a -> (State# RealWorld -> (# State# RealWorld, a #))
 unIO (IO x) = x
 
-pattern Raw, Out, World, Await :: World e -> (Void `L` World) e
+pattern Raw, Out, World, Await :: World e -> (World `L` World `T` Void) e
 pattern Raw e = Labeled e
 pattern Out e = Labeled e
 pattern World e = Labeled e
